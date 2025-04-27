505929c33c63b591c46155046073da424fae9a85
Mark <mallenr@gmail.com>
2025-04-27 14:57:25 -0400

    Format code

--- woo_remove_category_count.php
+++ woo_remove_category_count.php
@@ -1,7 +1,11 @@
 <?php
-// Remove the category count for WooCommerce categories
+// Remove the category count for WooCommerce categories.
+
 //added
 add_filter( 'woocommerce_subcategory_count_html', '__return_null' );
+
 remove_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_add_to_cart' );
+
 remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30 );
+
 add_filter( 'woocommerce_get_price_html', 'remove_price');
